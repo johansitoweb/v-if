ef620d9d9c710b46ba3babe5821a8157726adf3b
Johan Antonio <147890607+johansitoweb@users.noreply.github.com>
2024-12-20 20:12:26 -0400

    los v-if

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('Hello World!')
+</script>
+<template>
+<p v-if=" 4 > 0">
+  Esto es verdadero
+</p>
+
+<p v-else>
+  Esto es falso
+</p>
+</template>
+
+
+
+
+
+
+
+
+
+
+
+
+
